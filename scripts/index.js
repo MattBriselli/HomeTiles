d6c349e858159d0742dd3d0f549ab1067f98c43f
Matt Briselli <MattBriselli@gmail.com>
2017-11-11 18:53:23 -0500

    new libraries and permissions

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,48 @@
 requirejs([
     "node_modules/jquery/src/jquery",
-    "node_modules/moment/moment"
+    "node_modules/underscore/underscore",
+    "node_modules/moment/moment",
+    "node_modules/sortablejs/Sortable",
     ],
     function(
         $,
-        moment
+        _,
+        moment,
+        Sortable
     ){
         var _permissions,
+            _stored,
+            _cards,
             /*
              * a brslli labs application
              * made by Matt Briselli
              * brslli.com
              */
             _init = function _init() {
-                _weatherCall();
-                _bindListener()
+                _loader();
+                _bindListener();
             },
             _weatherCall = function _weatherCall() {
+                _stored["zipcode"];
                 $.ajax({
                     url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a77b08a6d315fb4d974c16345ae1ba70",
                     type: "GET"
                 })
                 .done(function(wData) {
-                    console.log(wData);
+                    console.log(wData, _permissions);
                 })
                 .fail(function(error) {
                     console.log("ERROR");
                     console.log("FAILED TO GET WEATHER data");
                 });
             },
+            _stockLoader = function _stockLoader() {
+                var apiKey = "LRRFZ6VVIF8ODL1D";
+            },
             _bindListener = function _bindListener() {
                 $(".editMode .fa-pencil").on("click", function() {
                     if ($(".editPanel").css("right") == "0px" || $(".editPanel").css("right") == "0%") {
-                        direction = '-16%'
+                        direction = '-19%';
                     } else {
                         direction = '0%';
                     }
@@ -42,6 +52,22 @@ requirejs([
                 });
                 $(".editMode .fa").on("mouseover", function(e) {
 
+                });
+            },
+            _loader = function _loader() {
+                chrome.storage.sync.get(null, function(items) {
+                    //null loads all of the data
+                    _stored = items;
+                    if (items) {
+                        console.log(items);
+                    }
+                });
+                chrome.storage.sync.remove("zipcode", function(items) {
+                    //null loads all of the data
+                    _stored = items;
+                    if (true) {
+
+                    }
                 });
             };
         $(document).ready(function() {
