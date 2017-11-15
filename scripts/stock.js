d64db37c05bda55d372d1cb739c28ad52823a3cb
Matt Briselli <briselli@adobe.com>
2017-11-15 14:14:58 -0500

    new files

---
+++ scripts/stock.js
@@ -0,0 +1,88 @@
+define([
+    "jquery",
+    "underscore",
+    "moment",
+    "Sortable",
+    "tile",
+    "text!../tiles/stock.html"
+    ],
+    function(
+        $,
+        _,
+        moment,
+        Sortable,
+        tileJs,
+        tmpl
+    ){
+        var _stored,
+            _prefs,
+            _configs,
+            _tmpl,
+            /*
+             * a brslli labs application
+             * made by Matt Briselli
+             * brslli.com
+             */
+            _init = function _init(index, stored, prefs, configs) {
+                _stored = stored;
+                _prefs = prefs;
+                _configs = configs;
+                _tmpl = tmpl;
+                var url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=",
+                    keys = ["AK45C9WF40HN3PRW", "LYSB01MBM0109645"];
+                url += "GOOG" +"&interval=60min&apikey=" + keys[0];
+                $.ajax({
+                    url: url,
+                    type: "GET"
+                })
+                .done(function(data) {
+                    console.log(data)
+                    var dataSet = data["Time Series (60min)"],
+                        minD = Number.MAX_SAFE_INTEGER,
+                        maxD = 0,
+                        newest = _.values(dataSet)[0];
+
+                    for (var j in dataSet) {
+                        var targ = dataSet[j];
+
+                        if (targ["3. low"] < minD) {
+                            minD = targ["3. low"];
+                        }
+                        if (targ["2. high"] > maxD) {
+                            maxD = targ["2. high"];
+                        }
+                    }
+
+                    console.log(newest, minD, maxD);
+
+                    tileStyler(data);
+                })
+                .fail(function(error) {
+                    console.log('ERROR');
+                    console.log(error);
+                    console.log('FAILED TO LOAD STOCK DATA');
+                });
+
+                function tileStyler(wData) {
+                    var tile = $(_tmpl),
+                        tF = tile.find(".top .front");
+                    if (_configs && _configs["stock"]) {
+                        for (var i=0; i<_configs["stock"].length; i++) {
+
+                        }
+                    }
+
+                    tileJs(tile, index);
+                }
+            },
+            _dataStore = function _dataStore(obj, index) {
+                chrome.storage.sync.set(obj, function() {
+                    //null loads all of the data
+                    console.log("STORED: "+obj+" and "+index);
+                });
+            };
+
+        return {
+            init: _init
+        };
+});
---
+++ scripts/tile.js
@@ -0,0 +1,49 @@
+define([
+    "jquery",
+    "underscore",
+    "moment",
+    "Sortable"
+    ],
+    function(
+        $,
+        _,
+        moment,
+        Sortable
+    ){
+        var _stored,
+            _prefs,
+            _configs,
+            /*
+             * a brslli labs application
+             * made by Matt Briselli
+             * brslli.com
+             */
+            _init = function _init(tile, index) {
+                var newTile = $(tile).find(".tile");
+                newTile.find(".fa").on("click", function() {
+                    $(newTile).find(".front, .back").toggle();
+                });
+                newTile.attr("data-index", index);
+                newTile.css("backgroundColor", "yellow");
+
+                if ($(".tile[data-index='"+index+"']").length != 0) {
+                    //a simple replacement
+                    $(".tile[data-index='"+index+"']").replaceWith(newTile);
+                } else {
+                    //order matters, the user sets that preference
+                    if (index == 0) {
+                        //0 will always be first
+                        $(".tileBody").prepend(newTile);
+                    } else if ($(".tile[data-index='"+parseInt(index-1)+"']").length != 0) {
+                        $(".tile[data-index='"+parseInt(index-1)+"']").after(newTile);
+                    } else if ($(".tile[data-index='"+parseInt(index+1)+"']").length != 0) {
+                        $(".tile[data-index='"+parseInt(index+1)+"']").before(newTile);
+                    } else {
+                        //very weird order, just stick it at the end and hope for the best
+                        $(".tileBody").append(newTile);
+                    }
+                }
+            };
+
+    return _init;
+});
