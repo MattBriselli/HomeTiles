4842e1d03e6be300e0f6f8541a768f2e70189cc2
Matt Briselli <briselli@adobe.com>
2018-02-26 08:32:26 -0500

    minor

--- scripts/weather.js
+++ scripts/weather.js
@@ -251,8 +251,6 @@ define([
                         i++;
                     }
 
-                    console.log(i);
-
                     delete _stored["weather"][i];
                     delete _stored["configs"]["weather"][i];
                     //need to delete the last one as the rest have shifted up
