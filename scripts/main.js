966584b32d4decc6db60b70fd79089ba2652c764
Matt Briselli <briselli@adobe.com>
2018-06-08 14:36:50 -0400

    added d3

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ require.config({
         moment: "../node_modules/moment/moment",
         Sortable: "../node_modules/sortablejs/Sortable",
         underscore: "../node_modules/underscore/underscore-min",
+        d3: "../libraries/d3.v5.min",
         text: "../node_modules/text/text",
         tab: "index"
     }
--- scripts/stock.js
+++ scripts/stock.js
@@ -3,6 +3,7 @@ define([
     "underscore",
     "moment",
     "Sortable",
+    "d3",
     "tile",
     "text!../tiles/stock.html"
     ],
@@ -11,6 +12,7 @@ define([
         _,
         moment,
         Sortable,
+        d3,
         tileJs,
         tmpl
     ){
@@ -31,14 +33,14 @@ define([
 
                 var url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=",
                     keys = ["AK45C9WF40HN3PRW", "LYSB01MBM0109645"];
-                url += "GOOG" +"&interval=60min&apikey=" + keys[0];
+                url += "GOOG" +"&interval=5min&apikey=" + keys[0];
                 $.ajax({
                     url: url,
                     type: "GET"
                 })
                 .done(function(data) {
-                    console.log(data)
-                    var dataSet = data["Time Series (60min)"],
+                    _dataDraw(data);
+                    var dataSet = data["Time Series (5min)"],
                         minD = Number.MAX_SAFE_INTEGER,
                         maxD = 0,
                         newest = _.values(dataSet)[0];
@@ -54,7 +56,7 @@ define([
                         }
                     }
 
-                    console.log(newest, minD, maxD);
+                    console.log(data, Object.values(dataSet).length, newest, minD, maxD);
 
                     _tileStyler(data, index);
                 })
@@ -71,6 +73,9 @@ define([
 
                 }
                 tileJs(tile, index);
+            },
+            _dataDraw = function _dataDraw(data) {
+                console.log(d3.select("svg"));
             },
             _dataStore = function _dataStore(obj, index) {
                 chrome.storage.sync.set(obj, function() {
