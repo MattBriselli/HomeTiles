df2fdae1ad4b4d79fd8d6ae4872b0bad2c49ac45
Matt Briselli <MattBriselli@gmail.com>
2018-09-03 20:59:56 -0600

    error ordering

--- scripts/tile.js
+++ scripts/tile.js
@@ -16,36 +16,17 @@ define([
          * brslli.com
          */
         var _init = function _init(tile, index) {
-            var newTile = _newTileSet(tile, index);
+            var newTile = _newTile(tile, index);
 
             if ($(".tile[data-index='"+index+"']").length != 0) {
                 //a simple replacement
                 $(".tile[data-index='"+index+"']").replaceWith(newTile);
             } else {
-                //order matters, the user sets that preference
-                if (index == 0) {
-                    //0 will always be first
-                    $(".tileBody").prepend(newTile);
-                } else {
-                    var added = false;
-                    $(".tile").each(function() {
-                        var thisIndex = $(this).data("index");
-                        if (thisIndex == index - 1 && !added) {
-                            $(this).after(newTile);
-                            added = true;
-                        } else if (!added && thisIndex > index) {
-                            $(this).before(newTile);
-                            added = true;
-                        }
-                    });
-                    if (!added) {
-                        $(".tileBody").append(newTile);
-                    }
-                }
+                _tileSet(newTile, index);
             }
         },
         _error = function _error(tile, index, error) {
-            var newTile = _newTileSet(tile, index);
+            var newTile = _newTile(tile, index);
             if ($(tile).find(".weather").length > 0) {
                 //no data, throw an error msg
                 var place = (error["city"] ? error["city"] + ", " + error["country"] : error["zipcode"]);
@@ -63,14 +44,11 @@ define([
             } else {
                 newTile.find(".front .top").html("<div class='error'><div>Error Loading Data for</div><div>" +
                     error["stock"] + "</div></div>");
-                if ($(".tile[data-index='"+index+"']").length != 0) {
-                    $(".tile[data-index='"+index+"']").replaceWith(newTile);
-                } else {
-                    $(".tileBody").append(newTile);
-                }
+
+                _tileSet(newTile, index);
             }
         },
-        _newTileSet = function _newTileSet(tile, index) {
+        _newTile = function _newTile(tile, index) {
             var newTile = $(tile).find(".tile");
             newTile.find(".fa-pencil").on("click", function() {
                 $(newTile).find(".front, .back, .fa-trash-o").toggle();
@@ -78,6 +56,27 @@ define([
             });
             newTile.attr("data-index", index);
             return newTile;
+        },
+        _tileSet = function _tileSet(newTile, index) {
+            if (index == 0) {
+                //0 will always be first
+                $(".tileBody").prepend(newTile);
+            } else {
+                var added = false;
+                $(".tile").each(function() {
+                    var thisIndex = $(this).data("index");
+                    if (thisIndex == index - 1 && !added) {
+                        $(this).after(newTile);
+                        added = true;
+                    } else if (!added && thisIndex > index) {
+                        $(this).before(newTile);
+                        added = true;
+                    }
+                });
+                if (!added) {
+                    $(".tileBody").append(newTile);
+                }
+            }
         };
 
 
